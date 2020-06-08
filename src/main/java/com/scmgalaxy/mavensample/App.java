f7958c37656303bc774d011172f6aeeb4257b1da
rutaborkar <33936960+rutaborkar@users.noreply.github.com>
2020-06-08 20:46:43 +0530

    Update App.java

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -9,8 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Nerds!!" );
-        System.out.println( "Hello Nerds!!" );
-        System.ou.println( "Hello Nerds!!" );
+        
     }
     
     
